c7dd83f928f036d221c5fa0d0c54b5f1aa06ab07
Eduardo Ferreira <2keduardoferreira@gmail.com>
2026-03-11 00:14:26 -0300

    finaliza lógica de IR e integra relatório detalhado ao layout

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const botaoCalcular = document.getElementById("btn_calcular");
 botaoCalcular.addEventListener("click", function(){
 
     let montanteTotal = 0;
+    
+    let taxaIr = 0.225;
 
     let investimentoInicial = Number(document.getElementById("investimento_inicial").value);
 
@@ -30,15 +32,58 @@ botaoCalcular.addEventListener("click", function(){
 
     let taxaMensal = (1 + (rendimentoAnualBruto)/100 ) ** (1/12) - 1;
 
+
+    let montanteTotalInvestido = investimentoInicial + (investimentoMensal * tempoFinal);
+
     montanteTotal = investimentoInicial
 
     for(let i = 0; i < tempoFinal; i++){
         montanteTotal = montanteTotal * (1 + taxaMensal);
         montanteTotal = montanteTotal + investimentoMensal;
     }
+
+    let qtd_dias;
+    qtd_dias = tempoFinal * 30
+
+    if (qtd_dias > 720)
+        taxaIr = 0.15;
+    else if(qtd_dias >= 361)
+        taxaIr = 0.175;
+    else if(qtd_dias >= 181)
+        taxaIr = 0.2;
+    else 
+        taxaIr = 0.225;
+
+    let rendimentoBruto = montanteTotal - montanteTotalInvestido;
+
+    let rendimentoLiquido = rendimentoBruto * (1-taxaIr);
+
+    let valorFinalLiquido = montanteTotalInvestido + rendimentoLiquido;
+
+    let impostoRenda = rendimentoBruto - rendimentoLiquido;
+
+    valorFinalLiquido = valorFinalLiquido.toLocaleString('pt-br', {style:"currency", currency: "BRL"});
+
+    montanteTotal = montanteTotal.toLocaleString('pt-br', {style: "currency", currency: "BRL"});
+
+    montanteTotalInvestido = montanteTotalInvestido.toLocaleString('pt-br', {style: "currency", currency: "BRL"});
+
+    rendimentoLiquido = rendimentoLiquido.toLocaleString('pt-br', {style:"currency", currency: "BRL"});
+
+    rendimentoBruto = rendimentoBruto.toLocaleString('pt-br', {style:"currency", currency: "BRL"});
+
+    impostoRenda = impostoRenda.toLocaleString('pt-br', {style: "currency", currency: "BRL"});
+
+    document.getElementById("resultado_valor_bruto").textContent = montanteTotal;
+    document.getElementById("valor_investido").textContent = montanteTotalInvestido;
+    document.getElementById("rend_bruto").textContent = rendimentoBruto;
+    document.getElementById("rend_liquido").textContent = rendimentoLiquido;
+    document.getElementById("imposto").textContent = impostoRenda;
+    document.getElementById("valor_liquido").textContent = valorFinalLiquido;
     
-    let montanteFormatado = montanteTotal.toFixed(2);
-    document.getElementById("texto_resultado").innerText = `O montante final é : R$ ${montanteFormatado}`;
+
+    document.getElementById("relatorio_investimento").style.display = "block";
+   
 });
 
 
